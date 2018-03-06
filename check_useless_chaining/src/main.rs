8ce54cade44b15d87e2eedeff0109b2693205922
chao450 <lin.chao.xz@gmail.com>
2018-03-06 22:41:39 +0100

    remove oak

--- check_useless_chaining/src/main.rs
+++ check_useless_chaining/src/main.rs
@@ -9,14 +9,25 @@ grammar! useless_chaining {
   test2 = &(&"a") // &"a"
   test3 = !(&"a") // !"a"
   test4 = &(!"a") // !"a"
-
   //test5 = ("a"*)* // infinite loop -> deja detectee
   test6 = ("a"+)+ // "a"+
   test7 = ("a"+)* // "a"+
   //test8 = ("a"*)+ // infinite loop -> deja detectee
-  test9 = &(!"a")
-  test10 = &(test9)
-  test11 = &(test10)
+
+  test9 = !"a"
+  test10 = !test9
+
+  test11 = &"a"
+  test12 = &test11
+
+  test13 = !test11
+
+  test14 = &test9
+
+  test15 = "a"+
+  test16 = test15+
+
+  test17 = test15*
 }
 
 fn main() {
